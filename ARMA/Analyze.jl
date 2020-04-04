24fe4c232f169eb4d10c1c62e5c058002a367e01
Michael Creel <michael.creel@uab.es>
2020-04-04 10:20:52 +0200

    rename to ARMA

--- ARMA/Analyze.jl
+++ ARMA/Analyze.jl
@@ -1,16 +1,11 @@
 using Pkg
 Pkg.activate("../")
 using Econometrics, DelimitedFiles, Statistics
-θtrue = [0.95, 0.5, 1.0]
-bias = zeros(3,1)
-rmse = zeros(3,1)
-ci99 = zeros(3,1)
-ci95 = zeros(3,1)
-ci90 = zeros(3,1)
+θtrue = [0.95,0.5,1.0]
 files = [
-    "raw_statistic"]
-
-for j = 1:1
+    "baseline_NN",
+    "no_Jacobian_NN"];
+for j = 1:2
     d = readdlm(files[j])
     error = d[:,1:3] .- θtrue'
     b = mean(error, dims=1) ./θtrue'
@@ -18,14 +13,8 @@ for j = 1:1
     c99 = mean(d[:,10:12], dims=1)
     c95 = mean(d[:,13:15], dims=1)
     c90 = mean(d[:,16:18], dims=1)
-    bias[:,j] = b
-    rmse[:,j] = r
-    ci99[:,j] = c99
-    ci95[:,j] = c95
-    ci90[:,j] = c90
+    names = ["rmse", "bias", "c90","c95","c99"]
+    prettyprint([r' b' c90' c95' c99'],names,"")
 end    
-prettyprint(bias)
-prettyprint(rmse)
-prettyprint(ci99)
-prettyprint(ci95)
-prettyprint(ci90)
+
+
