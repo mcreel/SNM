6f90fbc0f93665407238526f6f930c35b57a02fc
Michael Creel <michael.creel@uab.es>
2020-05-28 09:22:21 +0200

    minor

--- MakeTables.jl
+++ MakeTables.jl
@@ -13,6 +13,7 @@ function getresults()
         if i==1
             dir="SV/"
             θtrue = [exp(-0.736/2.0), 0.9, 0.363]
+@show θtrue
         elseif i==2
             dir="DPD/"
             θtrue = [0.6, 1.0, 2.0]
@@ -30,8 +31,8 @@ function getresults()
         for j = 1:3
             d = readdlm(dir*files[j])
             error = d[:,1:3] .- θtrue'
-            b[:,j] = mean(error, dims=1) ./θtrue'
-            r[:,j] = sqrt.(mean(error.^2.0, dims=1)) ./θtrue'
+            b[:,j] = mean(error, dims=1) #./θtrue'
+            r[:,j] = sqrt.(mean(error.^2.0, dims=1)) #./θtrue'
             c99[:,j] = mean(d[:,4:6], dims=1)
             c95[:,j] = mean(d[:,7:9], dims=1)
             c90[:,j] = mean(d[:,10:12], dims=1)
