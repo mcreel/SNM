212e7a75b4987251b843118727fa4dee56f9fc3b
mcreel <michael.creel@uab.es>
2020-07-17 17:10:16 +0200

    set project to SV

--- RunProject.jl
+++ RunProject.jl
@@ -1,4 +1,4 @@
-project="JD"  # set to one of the projects in examples: SV, DPD, ARMA, MN
+project="SV"  # set to one of the projects in examples: SV, DPD, ARMA, MN
 run_title = "working" # Monte Carlo results written to this file
 mcreps = 1000 # how many reps?
 
