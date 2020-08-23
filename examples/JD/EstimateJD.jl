f200b47d2287ec3e652e06bce54c9514f5bfc1e3
mcreel <michael.creel@uab.es>
2020-08-23 08:16:23 +0200

    simulation estimation third round JD

--- examples/JD/EstimateJD.jl
+++ examples/JD/EstimateJD.jl
@@ -12,7 +12,7 @@ function main()
 # generate the trained net: comment out when done for the chosen model
 nParams = size(PriorSupport()[1],1)
 TrainingTestingSize = Int64(nParams*2*1e4) # 20,000 training and testing for each parameter
-MakeNeuralMoments(auxstat, TrainingTestingSize) # already done for the 4 examples
+#MakeNeuralMoments(auxstat, TrainingTestingSize) # already done for the 4 examples
 @load "neural_moments.bson" NNmodel transform_stats_info
 m = NeuralMoments(TrueParameters(), auxstat, 1, NNmodel, transform_stats_info)
 @time chain, θhat = MCMC(m, auxstat, NNmodel, transform_stats_info; verbosity=true, nthreads=10, rt=0.1)
