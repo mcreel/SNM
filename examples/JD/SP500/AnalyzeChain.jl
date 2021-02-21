fc618ffc2ed4b0d1597ec9638ecaee0a5b0ba798
mcreel <michael.creel@uab.es>
2021-02-21 12:53:32 +0100

    minor

--- examples/JD/SP500/AnalyzeChain.jl
+++ examples/JD/SP500/AnalyzeChain.jl
@@ -1,39 +1,10 @@
-using SimulatedNeuralMoments, Flux, MCMCChains, StatsPlots, DelimitedFiles, Econometrics
-using BSON:@save
-using BSON:@load
-using DelimitedFiles
-#=
-# get the things to define the structure for the model
-# For your own models, you will need to supply the functions
-# found in MNlib.jl, using the same formats
-include("JDlib.jl")
-lb, ub = PriorSupport()
-
-# fill in the structure that defines the model
-model = SNMmodel("SP500 estimation", lb, ub, InSupport, Prior, PriorDraw, auxstat)
-
-# train the net, and save it and the transformation info
-#nnmodel, nninfo = MakeNeuralMoments(model)
-#@save "neuralmodel.bson" nnmodel nninfo  # use this line to save the trained neural net 
-@load "neuralmodel.bson" nnmodel nninfo # use this to load a trained net
-data = readdlm("SP500.txt")
-data = Float64.(data[2:end,2:end])
-rets = data[:,1]
-RV = data[:,2]
-BV = data[:,3]
-z = auxstat(rets, RV, BV)
-m = mean(min.(max.(Float64.(nnmodel(TransformStats(z, nninfo)')),model.lb),model.ub),dims=2)
-# draw a chain of length 10000, and get the extremum estimator
-chain, θhat = MCMC(m, 10000, model, nnmodel, nninfo, verbosity=true)
-# save visualize results
-writedlm("chain", chain)
-writedlm("thetahat", θhat)
-=#
+using MCMCChains, StatsPlots, DelimitedFiles, Econometrics
 chain = readdlm("chain")
 chain = chain[500:end,:] # drop burnin period
 chn = Chains(chain)
 display(chn)
 plot(chn)
+#=
 savefig("chain.png")
 savefig(npdensity(chain[:,1]), "mu.png")
 savefig(npdensity(chain[:,2]), "kappa.png")
@@ -43,3 +14,4 @@ savefig(npdensity(chain[:,5]), "rho.png")
 savefig(npdensity(chain[:,6]), "lambda0.png")
 savefig(npdensity(chain[:,7]), "lambda1.png")
 savefig(npdensity(chain[:,8]), "tau.png")
+=#
