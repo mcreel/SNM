7ce35f52c1211081302864c0f7d71f2311b84d8e
Michael Creel <michael.creel@uab.es>
2021-02-22 10:29:06 +0100

    small fix

--- examples/JD/SP500/README.jl
+++ examples/JD/SP500/README.jl
@@ -2,7 +2,7 @@ This directory holds an example of estimation of the jump diffusion model
 ![model](https://github.com/mcreel/SNM/blob/master/examples/JD/SimulationEstimation/model.png)
 
 using data on S&P500 returns from Dec. 2013 - Dec. 2017. The data is pictured in  
-![returns](https://github.com/mcreel/SNM/blob/master/examples/JD/SP500/returns.svg)
+![returns](https://github.com/mcreel/SNM/blob/master/examples/JD/SP500/returns.png)
 
 Log price, p, is assumed to be measured with a N(0,\tau^2) error. The results are in the .png files. For example, for the parameter tau, the standard deviation of measurement error in observed returns, the marginal posterior is
 is in the following figure. We see that there is strong evidence in favor of measurement error being a factor.
