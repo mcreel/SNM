889df88201957aa67a734b92be2880a6b55103f5
Michael Creel <michael.creel@uab.es>
2021-02-23 13:02:01 +0100

    move things around to clear up JD example

--- examples/JD/JDlib.jl
+++ examples/JD/JDlib.jl
@@ -1,4 +1,4 @@
-using DifferentialEquations, Statistics, Econometrics
+using DifferentialEquations, Statistics
 
 function Diffusion(μ,κ,α,σ,ρ,u0,tspan)
     f = function (du,u,p,t)
@@ -27,16 +27,22 @@ function TrueParameters()
     return [μ, κ, α, σ, ρ, λ0, λ1, τ]
 end
 
-
 function PriorSupport()
     lb = [-0.1, 0.001, -3.0, 0.01, -0.99, -0.02,  3.0, -0.02]
     ub = [0.1,  0.5, 1.0, 2.0,  0.0, 0.05, 6.0, 0.05]
     lb,ub
 end    
 
-function PriorMean()
+# check if parameter is in support.
+function InSupport(θ)
     lb,ub = PriorSupport()
-    (ub + lb) ./ 2.0
+    all(θ .>= lb) & all(θ .<= ub)
+end
+
+# prior checks that we're in the bounds, and that the unconditional std. dev. of log vol is not too high
+# returns 1 if this is true, zero otherwise. Value is not important, as it's constant
+function Prior(θ)
+    InSupport(θ) ? 1.0 : 0.0
 end
 
 function PriorDraw()
@@ -44,51 +50,54 @@ function PriorDraw()
     θ = (ub-lb).*rand(size(lb,1)) + lb
 end    
 
-# prior checks that we're in the bounds, and that the unconditional std. dev. of log vol is not too high
-# returns 1 if this is true, zero otherwise. Value is not important, as it's constant
-function Prior(θ)
+
+function PriorMean()
     lb,ub = PriorSupport()
-    a = 0.0
-    if (all(θ .>= lb) & all(θ .<= ub))
-        a = 1.0
-    end
-    return a
+    (ub + lb) ./ 2.0
 end
 
-function dgp(θ::Array{Float64},reps::Int64)
-TradingDays = 1000*reps    
+
+function solver(θ)
+    TradingDays = 1000    
+    Days = TradingDays+Int(TradingDays/5*2)+1 # add weekends, plus a day for lag
+    MinPerDay = 1440 # minutes per day
+    MinPerTic = 5 # minutes between tics, lower for better accuracy
+    tics = Int(MinPerDay/MinPerTic) # number of tics in day
+    dt = 1/tics # divisions per day
+    closing = Int(round(6.5*60/MinPerTic)) # tic at closing
+    # parameters
+    μ, κ, α, σ, ρ, λ0, λ1, τ = θ
+    u0 = [μ; α]
+    prob = Diffusion(μ, κ, α, σ, ρ, u0, (0.0,Days))
+    ## jump in log price
+    rate(u,p,t) = λ0.*(λ0>0.0) # the prior allows for negative rate, to allow an accumulation at zero
+    # jump is random sign times  λ1 times current st. dev.
+    affect1!(integrator) = (integrator.u[1] = integrator.u[1].+rand([-1.0,1.0]).*λ1.*exp(integrator.u[2]./2.0))
+    jump = ConstantRateJump(rate,affect1!)
+    jump_prob = JumpProblem(prob,Direct(), jump)
+    sol = solve(jump_prob,SRIW1(), dt=dt, adaptive=false)
+end
+
+function dgp(θ::Array{Float64})
+TradingDays = 1000    
 Days = TradingDays+Int(TradingDays/5*2)+1 # add weekends, plus a day for lag
 MinPerDay = 1440 # minutes per day
 MinPerTic = 5 # minutes between tics, lower for better accuracy
 tics = Int(MinPerDay/MinPerTic) # number of tics in day
 dt = 1/tics # divisions per day
 closing = Int(round(6.5*60/MinPerTic)) # tic at closing
-# parameters
+# solve the diffusion
+sol = solver(θ)
+
+# simulate
 μ, κ, α, σ, ρ, λ0, λ1, τ = θ
-u0 = [0.0;α]
-prob = Diffusion(μ, κ, α, σ, ρ, u0, (0.0,Days))
-## jump in log price
-rate(u,p,t) = λ0.*(λ0>0.0) # the prior allows for negative rate, to allow an accumulation at zero
-# jump is random sign times  λ1 times current st. dev.
-affect1!(integrator) = (integrator.u[1] = integrator.u[1].+rand([-1.0,1.0]).*λ1.*exp(integrator.u[2]./2.0))
-jump = ConstantRateJump(rate,affect1!)
-jump_prob = JumpProblem(prob,Direct(), jump)
-sol = solve(jump_prob,SRIW1(), dt=dt, adaptive=false)
-# find when jumps occur
-#jump = sol.t[2:end].==sol.t[1:end-1] # find times where jumps occur
-#jump[1] = 0 # this is always true, for some reason, set it false
-#jump = vcat(false,jump) 
-#jumptimes = sol.t[jump] .* TradingDays ./ Days 
 lnPs = [sol(t)[1] for t in dt:dt:Days]
 lnPs = lnPs + τ .* randn(size(lnPs)) # add measurement error
-#vol = [sol(t)[2] for t in dt:dt:Days]
 # get log price at end of trading days. We will compute lag, so loose first
 lnPtrading = zeros(TradingDays+1)
 #Volatility = zeros(TradingDays+1) # real latent volatility
 RV = zeros(TradingDays+1)
 BV = zeros(TradingDays+1)
-ret0 = zeros(TradingDays+1) # returns at open
-Monday = zeros(TradingDays+1)
 DayofWeek = 0 # counter for day of week
 TradingDay = 0 # counter for trading days
 Day = 0
@@ -108,19 +117,13 @@ lnPlag = 0.0
             lnPlag = lnP # update lag, this is inter-day for the first
             t2 = t1 # one lag
             t1 = abs(ret) # current
-            # compute interday initial return
-            if tic == 1
-                ret0[TradingDay] = ret
-            end
             # RV measures
             if tic > 1
-                RV[TradingDay]+=(ret^2.0)
+                RV[TradingDay]+=(ret*ret)
                 BV[TradingDay] += t1*t2
             end
         end    
         lnPtrading[TradingDay] = lnPs[(Day-1)*tics+closing]
-        #Volatility[TradingDay] = exp(0.5*vol[(Day-1)*tics+closing]) # true volatility
-        Monday[TradingDay] = (DayofWeek == 1)
     end
     if DayofWeek==7 # restart the week if Sunday
         DayofWeek = 0
@@ -130,73 +133,58 @@ rets = lnPtrading[2:end]-lnPtrading[1:end-1] # inter-day returns
 RV = RV[2:end]
 BV = (pi/2.0) .* BV
 BV = BV[2:end]
-#Volatility = Volatility[2:end]
-Monday = Monday[2:end]
-ret0 = ret0[2:end]
-return rets, RV, BV, ret0, Monday
+return rets, RV, BV
 end
 
 function auxstat(θ::Array{Float64}, reps::Int64)
-    not_ok = true
-    stats = 0.0
-    while not_ok
-        rets, RV, BV, ret0, Monday = dgp(θ,reps)
-        stats = auxstat(rets, RV, BV, reps)
-        not_ok = any(isnan.(stats))
-        if not_ok println("NaN in stats") end
+    stats = zeros(reps,25)
+    Threads.@threads    for i = 1:reps
+        rets, RV, BV = dgp(θ)
+        stats[i,:] = auxstat(rets, RV, BV)
     end
     return stats
 end
 
 # auxstats, given data (either simulated or real)
-function auxstat(rets::Array{Float64}, RV::Array{Float64}, BV::Array{Float64}, reps::Int64)
+function auxstat(rets::Array{Float64}, RV::Array{Float64}, BV::Array{Float64})
     RV = log.(RV)
     BV = log.(BV)
     jump = RV .> (1.5 .* BV)
     nojump = jump .== false
-    stats = zeros(reps,25)
     n = 1000
-    @inbounds Threads.@threads for rep = 1:reps
-        # split out data for this rep
-        retsr = rets[rep*n-n+1:rep*n]
-        RVr = RV[rep*n-n+1:rep*n]
-        BVr = BV[rep*n-n+1:rep*n]
-        jumpr = jump[rep*n-n+1:rep*n]
-        nojumpr = nojump[rep*n-n+1:rep*n]
-        # ensure variation
-        jumpr[1:2] .= true
-        nojumpr[1:2] .= true
-        # jump stats 
-        jumpsize = mean(RVr[jumpr]) - mean(BVr[jumpr])
-        jumpsize2 = std(retsr[jumpr]) - std(retsr[nojumpr])
-        njumps = mean(jumpr[3:end])
-        # ρ
-        X = [ones(n-1) BVr[2:end] BVr[1:end-1]]
-        y = retsr[2:end]
-        βrets = X\y
-        ϵrets = y-X*βrets
-        σrets = std(ϵrets)
-        κrets = std(ϵrets.^2.0)
-        # normal volatility: κ, α and σ
-        X = [ones(n-2) BVr[1:end-2] BVr[2:end-1]]
-        y = BVr[3:end]
-        βvol = X\y
-        ϵvol = y-X*βvol
-        σvol = std(ϵvol)
-        κvol = std(ϵvol.^2.0)
-        # jump size
-        X = [ones(n) jumpr BVr jumpr.*BVr]
-        y = RVr
-        βjump = X\y
-        ϵjump = y-X*βjump
-        σjump = std(ϵjump)
-        κjump = std(ϵjump.^2.0)
-        # jump frequency
-        qs = quantile(abs.(retsr),[0.5, 0.9])
-        qs2 = quantile(RVr,[0.5, 0.9])
-        qs3 = quantile(BVr,[0.5, 0.9])
-        stats[rep,:] = sqrt(n)*vcat(βrets, βvol, βjump,σrets, σvol, σjump,κrets, κvol, κjump, mean(RVr) - mean(BVr), jumpsize, jumpsize2, qs[2]/qs[1], qs2[2]/qs2[1], qs3[2]./qs3[1], qs2 ./ qs3, njumps)'
-    end
+    # ensure variation
+    jump[1:2] .= true
+    nojump[1:2] .= true
+    # jump stats 
+    jumpsize = mean(RV[jump]) - mean(BV[jump])
+    jumpsize2 = std(rets[jump]) - std(rets[nojump])
+    njumps = mean(jump[3:end])
+    # ρ
+    X = [ones(n-1) BV[2:end] BV[1:end-1]]
+    y = rets[2:end]
+    βrets = X\y
+    ϵrets = y-X*βrets
+    σrets = std(ϵrets)
+    κrets = std(ϵrets.^2.0)
+    # normal volatility: κ, α and σ
+    X = [ones(n-2) BV[1:end-2] BV[2:end-1]]
+    y = BV[3:end]
+    βvol = X\y
+    ϵvol = y-X*βvol
+    σvol = std(ϵvol)
+    κvol = std(ϵvol.^2.0)
+    # jump size
+    X = [ones(n) jump BV jump.*BV]
+    y = RV
+    βjump = X\y
+    ϵjump = y-X*βjump
+    σjump = std(ϵjump)
+    κjump = std(ϵjump.^2.0)
+    # jump frequency
+    qs = quantile(abs.(rets),[0.5, 0.9])
+    qs2 = quantile(RV,[0.5, 0.9])
+    qs3 = quantile(BV,[0.5, 0.9])
+    sqrt(n)*vcat(βrets, βvol, βjump,σrets, σvol, σjump,κrets, κvol, κjump, mean(RV) - mean(BV), jumpsize, jumpsize2, qs[2]/qs[1], qs2[2]/qs2[1], qs3[2]./qs3[1], qs2 ./ qs3, njumps)'
     # brets 1:3
     # bvol 4:6
     # bjump 7:10
@@ -214,7 +202,6 @@ function auxstat(rets::Array{Float64}, RV::Array{Float64}, BV::Array{Float64}, r
     # qs3 22
     # qs2/qs3 23-24 
     # njumps 25
-    return stats
 end
 
 
