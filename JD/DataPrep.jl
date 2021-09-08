399821cecf348f0020cca40adb6f210dc61a007b
Michael Creel <michael.creel@uab.es>
2021-09-08 11:01:10 +0200

    v0.3 data

--- JD/DataPrep.jl
+++ JD/DataPrep.jl
@@ -1,14 +1,16 @@
-using CSV, DataFrames, DataFramesMeta, StatsPlots, Dates, DelimitedFiles
+using CSV, DataFrames, DataFramesMeta, Plots, Dates, DelimitedFiles
 # download and unzip the data file, from
 # https://realized.oxford-man.ox.ac.uk/images/oxfordmanrealizedvolatilityindices.zip
+# read the data into a DataFrame
 data = CSV.read("oxfordmanrealizedvolatilityindices.csv",DataFrame)
+# select the desired columns
 data = data[:,[:Column1, :Symbol, :close_price, :rv10, :bv]]
 # set desired names
 rename!(data, :Column1 => :Date)
 rename!(data, :rv10 => :rv)
 # select desired index
 sp500 = @subset(data, :Symbol .== ".SPX")
-# drop times from date
+# drop times from date by taking the leading 10 characters (yyyy-mm-dd)
 sp500.Date = [sp500.Date[i][1:10] for i = 1:size(sp500,1)]
 # compute percentage returns
 sp500.rets = vcat(0.0,[100.0 *(log(sp500.close_price[i]) - log(sp500.close_price[i-1])) for i=2:size(sp500,1)])
@@ -18,11 +20,13 @@ sp500.bv = 10000.0 .* sp500.bv
 # select the date range
 sp500 = @subset(sp500, :Date .>= "2013-12-17") # these dates give 1000 obs.
 sp500 = @subset(sp500, :Date .<= "2017-12-05")
+# write out variables to plain text file
 data = sp500[:,[:rets, :rv, :bv]]
 data = Matrix{Float64}(data)
 writedlm("sp500.txt", data)
-plot(sp500.Date, sp500.rets, legend=false)
-savefig("returns.svg")
-plot(sp500.Date, [sp500.rv, sp500.bv], label=["rv" "bv"])
-savefig("volatility.svg")
+# some plots
+plot(sp500.rets, legend=false)
+savefig("returns.png")
+p = plot([sp500.rv, sp500.bv], label=["rv" "bv"])
+savefig("volatility.png")
 
--- JD/EstimateSP500.jl
+++ JD/EstimateSP500.jl
@@ -1,4 +1,5 @@
-using SimulatedNeuralMoments, Flux, MCMCChains, StatsPlots, DelimitedFiles, Optim, CSV, DataFrames
+#=
+#using SimulatedNeuralMoments, Flux, MCMCChains, StatsPlots, DelimitedFiles, Optim, CSV, DataFrames
 using BSON:@save
 using BSON:@load
 using DelimitedFiles
@@ -12,25 +13,28 @@ lb, ub = PriorSupport()
 model = SNMmodel("SP500 estimation", lb, ub, InSupport, Prior, PriorDraw, auxstat)
 
 # train the net, and save it and the transformation info
-nnmodel, nninfo = MakeNeuralMoments(model)
-@save "neuralmodel.bson" nnmodel nninfo  # use this line to save the trained neural net 
+#nnmodel, nninfo = MakeNeuralMoments(model)
+#@save "neuralmodel.bson" nnmodel nninfo  # use this line to save the trained neural net 
 @load "neuralmodel.bson" nnmodel nninfo # use this to load a trained net
 data = readdlm("sp500.txt")
 m = NeuralMoments(auxstat(data), model, nnmodel, nninfo)
 @show m
 m = min.(max.(lb,m),ub)
 @show m
-chain, P, tuning = MCMC(m, 10000, model, nnmodel, nninfo, verbosity=true, do_cue = true, tuningloops=2, nthreads=10, burnin=40, tuning=0.75)
+
+#chain, P, tuning = MCMC(m, 10000, model, nnmodel, nninfo, verbosity=true, do_cue = true, tuningloops=2, nthreads=10, burnin=40, tuning=0.75)
 # save visualize results
 writedlm("chain", chain)
 writedlm("P", P)
 writedlm("tuning", tuning)
+=#
+chain = readdlm("chain")
 chn = Chains(chain)
 display(chn)
 plot(chn)
 savefig("chain.png")
 
-#= do this part by hand, using saved chain, to eliminate dependency on Econometrics
+# do this part by hand, using saved chain, to eliminate dependency on Econometrics
 savefig(npdensity(chain[:,1]), "mu.png")
 savefig(npdensity(chain[:,2]), "kappa.png")
 savefig(npdensity(chain[:,3]), "alpha.png")
@@ -39,4 +43,4 @@ savefig(npdensity(chain[:,5]), "rho.png")
 savefig(npdensity(chain[:,6]), "lambda0.png")
 savefig(npdensity(chain[:,7]), "lambda1.png")
 savefig(npdensity(chain[:,8]), "tau.png")
-=#
+
